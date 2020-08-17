0141efaadea2c08fcdf3c0c29f7ed0ae804d0a60
Emily Krauss <e.krauss27@gmail.com>
2020-08-17 09:32:42 -0700

    configed for postgres db, auth working

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_15_184245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
